f76b7ba72e75523112586d8b6f0c9a6e97f81afc
LilDad <bezhuraedgar@gmail.com>
2018-11-14 00:42:20 +0200

    Init2

---
+++ features/checkout_forms.feature
@@ -0,0 +1,4 @@
+Feature: Completing the checkout forms
+
+  Scenario:
+    Given I am on the site
---
+++ support/pages/home_page.rb
@@ -0,0 +1,12 @@
+module Pages
+  class HomePage
+  include PageObject
+
+  page_url 'https://www.thesiswritingservice.com/'
+
+  # as(a: 'Services')
+  # def services_tab
+  #   element(a: 'Services').click
+  # end
+  end
+end
